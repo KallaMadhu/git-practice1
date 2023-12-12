db4fc182e9af37d4e3c90ae2e5ea04d69f15be42
madhu <kallamadhu10@gmail.com>
2023-12-13 00:11:33 +0530

    second modification

--- One.java
+++ One.java
@@ -1,9 +1,13 @@
 
 class One
 {
+    
     static int add(int a,int b)
     {return a+b;}
+    static int sub(int a,int b)
+    {return a-b;}
     public static void main(String[] args) {
         System.out.println(add(100,200));
+        System.out.println(sub(100,200));
     }
 }
